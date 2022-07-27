6c370dec4bae8f067b969f61e68a14087bab01a7
The MMGen Project <mmgen@tuta.io>
2022-07-27 16:16:42 +0000

    new command: `mmnode-addrbal`
    Get balances for arbitrary addresses in the blockchain
    
    Testing:
    
        $ test/test.py -e regtest
    
    Example:
    
        # Top 10 Bitcoin addresses by balance (see https://blockchair.com/bitcoin/addresses)
        $ top10='
            34xp4vRoCGJym3xR7yCVPFHoCNxv4Twseo
            bc1qgdjqv0av3q56jvd82tkdjpy7gdp9ut8tlqmgrpmv24sq90ecnvqqjwvw97
            1LQoWist8KkaUXSPKZHNvEyfrEkPHzSsCd
            3LYJfcfHPXYJreMsASk2jkn69LWEYKzexb
            3M219KR5vEneNb47ewrPfWyb5jQ2DjxRP6
            bc1qazcm763858nkj2dj986etajv6wquslv8uxwczt
            37XuVSEpWW4trkfmvWzegTHQt7BdktSKUs
            1FeexV6bAHb8ybZjqQMjJrcCrHGW9sb6uF
            bc1qa5wkgaew2dkv56kfvj49j0av5nml45x9ek9hz6
            3Kzh9qAqVWQhEsfQz7zEQL1EuSx5tyNLNS'
    
        # Compact summary:
        $ mmnode-addrbal --tabular $top10
    
        # Full output:
        $ mmnode-addrbal $top10

--- test/test_py_d/ts_regtest.py
+++ test/test_py_d/ts_regtest.py
@@ -13,12 +13,24 @@ ts_regtest.py: Regtest tests for the test.py test suite
 """
 
 import os
+from mmgen.globalvars import g
 from mmgen.opts import opt
 from mmgen.util import die,gmsg
+from mmgen.protocol import init_proto
 from ..include.common import *
 from .common import *
 
 from .ts_base import *
+
+args1 = ['--bob']
+args2 = ['--bob','--rpc-backend=http']
+
+def gen_addrs(proto,network,keys):
+	from mmgen.tool.api import tool_api
+	tool = tool_api()
+	tool.init_coin(proto.coin,'regtest')
+	tool.addrtype = proto.mmtypes[-1]
+	return [tool.privhex2addr('{:064x}'.format(key)) for key in keys]
 
 class TestSuiteRegtest(TestSuiteBase):
 	'various operations via regtest mode'
@@ -30,6 +42,18 @@ class TestSuiteRegtest(TestSuiteBase):
 	deterministic = False
 	cmd_group = (
 		('setup',                     'regtest (Bob and Alice) mode setup'),
+		('sendto1',                   'sending funds to address #1 (1)'),
+		('sendto2',                   'sending funds to address #1 (2)'),
+		('sendto3',                   'sending funds to address #2'),
+		('addrbal_single',            'getting address balance (single address)'),
+		('addrbal_multiple',          'getting address balances (multiple addresses)'),
+		('addrbal_multiple_tabular1', 'getting address balances (multiple addresses, tabular output)'),
+		('addrbal_multiple_tabular2', 'getting address balances (multiple addresses, tabular, show first block)'),
+		('addrbal_nobal1',            'getting address balances (no balance)'),
+		('addrbal_nobal2',            'getting address balances (no balances)'),
+		('addrbal_nobal3',            'getting address balances (one null balance)'),
+		('addrbal_nobal3_tabular1',   'getting address balances (one null balance, tabular output)'),
+		('addrbal_nobal3_tabular2',   'getting address balances (one null balance, tabular, show first block)'),
 		('stop',                      'stopping regtest daemon'),
 	)
 
@@ -39,6 +63,8 @@ class TestSuiteRegtest(TestSuiteBase):
 			return
 		if self.proto.testnet:
 			die(2,'--testnet and --regtest options incompatible with regtest test suite')
+		self.proto = init_proto(self.proto.coin,network='regtest',need_amt=True)
+		self.addrs = gen_addrs(self.proto,'regtest',[1,2,3,4,5])
 
 		os.environ['MMGEN_TEST_SUITE_REGTEST'] = '1'
 
@@ -54,6 +80,40 @@ class TestSuiteRegtest(TestSuiteBase):
 		for s in ('Starting','Creating','Creating','Creating','Mined','Setup complete'):
 			t.expect(s)
 		return t
+
+	def sendto(self,addr,amt):
+		return self.spawn('mmgen-regtest',['send',addr,amt])
+
+	def sendto1(self): return self.sendto(self.addrs[0],'0.123')
+	def sendto2(self): return self.sendto(self.addrs[0],'0.234')
+	def sendto3(self): return self.sendto(self.addrs[1],'0.345')
+
+	def addrbal_single(self):
+		return self.spawn('mmnode-addrbal',args2+[self.addrs[0]])
+
+	def addrbal_multiple(self):
+		return self.spawn('mmnode-addrbal',args2+[self.addrs[1],self.addrs[0]])
+
+	def addrbal_multiple_tabular1(self):
+		return self.spawn('mmnode-addrbal',args2+['--tabular',self.addrs[1],self.addrs[0]])
+
+	def addrbal_multiple_tabular2(self):
+		return self.spawn('mmnode-addrbal',args1+['--tabular','--first-block',self.addrs[1],self.addrs[0]])
+
+	def addrbal_nobal1(self):
+		return self.spawn('mmnode-addrbal',args2+[self.addrs[2]])
+
+	def addrbal_nobal2(self):
+		return self.spawn('mmnode-addrbal',args2+[self.addrs[2],self.addrs[3]])
+
+	def addrbal_nobal3(self):
+		return self.spawn('mmnode-addrbal',args2+[self.addrs[4],self.addrs[0],self.addrs[3]])
+
+	def addrbal_nobal3_tabular1(self):
+		return self.spawn('mmnode-addrbal',args2+['--tabular',self.addrs[4],self.addrs[0],self.addrs[3]])
+
+	def addrbal_nobal3_tabular2(self):
+		return self.spawn('mmnode-addrbal',args1+['--tabular','--first-block',self.addrs[4],self.addrs[0],self.addrs[3]])
 
 	def stop(self):
 		if opt.no_daemon_stop:
